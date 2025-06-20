c5df692a177215e299d7d5ec73b1e22949bad1bf
orelhoso <88141984+orelhoso@users.noreply.github.com>
2025-06-19 21:01:09 -0300

    Previnir devolução acidental

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 function alterarStatus(id) {
     let idDoJogo = `game-${id}`;
     let jogo = document.getElementById(idDoJogo);
+    let nomeJogo = jogo.querySelector('p').textContent
     let botao = jogo.querySelector('a');
     let imagem = jogo.querySelector('div');
 
@@ -8,9 +9,11 @@ function alterarStatus(id) {
         botao.textContent = 'Devolver';
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
-        
+
     } else {
-        botao.textContent = 'Alugar';
+        confirmar = confirm(`Tem certeza que deseja devolver o jogo ${nomeJogo}?`);
+        if (confirmar)
+            botao.textContent = 'Alugar';
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
     }
