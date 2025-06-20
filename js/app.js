6a7e97c61897cddd80b2ee5cc5b5ffa26a3c3b19
orelhoso <88141984+orelhoso@users.noreply.github.com>
2025-06-19 21:42:44 -0300

    Contar quantos jogos estão alugados

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+let quantidadeAlugados = document.querySelectorAll('.dashboard__item__button--return').length; //conta quantos jogos estão alugados
+
 function alterarStatus(id) {
+
     let idDoJogo = `game-${id}`;
     let jogo = document.getElementById(idDoJogo);
     let nomeJogo = jogo.querySelector('p').textContent
@@ -6,15 +9,25 @@ function alterarStatus(id) {
     let imagem = jogo.querySelector('div');
 
     if (botao.textContent == 'Alugar') {
+
+        quantidadeAlugados++;
+
         botao.textContent = 'Devolver';
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
 
     } else {
+
         confirmar = confirm(`Tem certeza que deseja devolver o jogo ${nomeJogo}?`);
-        if (confirmar)
+        if (confirmar) {
+
+            quantidadeAlugados--;
+
             botao.textContent = 'Alugar';
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+        }
     }
+    
+    console.log(`Quantidade de jogos alugada atualmente: ${quantidadeAlugados}`)
 }
